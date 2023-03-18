d4ef435c5453f746054769e903ea940e0a760710
Kamrul Hasan <kamrulcse7@gmail.com>
2023-03-19 00:22:29 +0600

    os, and  path

---
+++ os_and_path/index.js
@@ -0,0 +1,33 @@
+
+// console.log(__dirname); // check current dirictory name
+// console.log(__filename); // check file name 
+
+// os //
+
+// const os = require("os");
+// console.log(os.userInfo()); // user info check
+// console.log(os.homedir()); //pc homedir check
+// console.log(os.hostname()); //hostname check
+// console.log(os.totalmem()); // total memory check
+// console.log(os.freemem()); // free memory check
+
+
+
+// we can use only pc userInfo then apply this way
+// const {userInfo} = require("os");
+// console.log(userInfo());
+
+// path //
+const path = require("path");
+// console.log(path.extname("info.txt")); // file name extension check
+
+const joinName = path.join(__dirname + "/views");
+const joinfileName = path.join(__dirname + "/../views");
+console.log(joinName);
+console.log(joinfileName);
+
+
+
+
+
+
