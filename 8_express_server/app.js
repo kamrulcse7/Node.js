3e9b97f23ef1ba721d4b41301e1a57d9636aadbf
Kamrul Hasan <kamrulcse7@gmail.com>
2023-04-01 19:38:26 +0600

    http method (get, post, put, delete)

--- 8_express_server/app.js
+++ 8_express_server/app.js
@@ -1,4 +1,32 @@
 const express = require('express');
 const app = express();
 
+// app.get("/", (req, res)=>{
+//     res.send("Home Page");
+// });
+
+// app.get("/about", (req, res)=>{
+//     res.send("About Page");
+// })
+
+app.get("/", (req, res) => {
+    res.send("Get Data");
+    res.end(); // end response
+});
+
+app.post("/", (req, res) => {
+    res.send("SignUp");
+    res.end();
+});
+
+app.put("/", (req, res) => {
+    res.send("Upadte Data");
+    res.end();
+});
+
+app.delete("/", (req, res) => {
+    res.send("Delete  Data");
+    res.end();
+});
+
 module.exports = app;
