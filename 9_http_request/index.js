91f1ac71f2e1a0176e2c0d4a23c2f9121ee48457
Kamrul Hasan <kamrulcse7@gmail.com>
2023-04-03 18:05:43 +0600

    get data from api body

--- 9_http_request/index.js
+++ 9_http_request/index.js
@@ -1,9 +1,16 @@
 // npm init -y
 // npm install express nodemon
+// npm install body-parser
 
 const express = require('express');
+var bodyParser = require('body-parser')
 const app = express();
 const PORT = 3001;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 // http://localhost:3001
 app.get("/", (req, res) => {
@@ -31,12 +38,18 @@ app.get("/id/:id/name/:name", (req, res) => {
 
 
 // get data from header 
-
 app.get("/get_data_from_header", (req, res) => {
     const id = req.header('id');
     const name = req.header('name');
     res.send(`id: ${id}, name: ${name}`)
-})
+});
+
+
+// http://localhost:3001/user/
+app.post("/user", (req, res)=>{
+    const name = req.body.name;
+    res.send(`Welcome ${name}`);
+});
 
 
 app.listen(PORT, () => {
